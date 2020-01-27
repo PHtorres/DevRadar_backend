cf48ec04862192c4d45fc600a435d5b90fbace02
PHtorres <58959268+PHtorres@users.noreply.github.com>
2020-01-27 10:20:11 -0300

    rotas

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
-const {Router} = require('express');
+const { Router } = require('express');
+const axios = require('axios');
+
 const routes = Router();
 
-routes.post('/users', (request, response) =>{
-    console.log(request.body);
-    return response.json({message: 'Hello World'});
+routes.post('/devs', async (request, response) => {
+
+    const { github_username } = request.body;
+    const resposta = await axios.get(`https://api.github.com/users/${github_username}`);
+    const { name = login, avatar_url, bio } = resposta.data;
+    console.log(name, avatar_url, bio, github_username);
+    return response.json({ message: 'Hello World' });
 });
 
 
